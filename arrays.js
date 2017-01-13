f88e7959aa5638eed3446de6b41812a9f347e4a5
George Higbie <gehigbie@gmail.com>
2017-01-13 16:38:38 -0700

    added new arrays

--- arrays.js
+++ arrays.js
@@ -9,3 +9,6 @@ colors[3] = "dark green";
 
 colors[10] = "violet"; //this leaves a lot of empty values "undefined"
 
+var friends = new Array(); //creates a new array
+
+var random = [34, true, "sammy"]; //can mix data values
