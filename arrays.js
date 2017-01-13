21cca2a2db14d9993e55db886156071aadfd8389
George Higbie <gehigbie@gmail.com>
2017-01-13 16:48:16 -0700

    added methods

--- arrays.js
+++ arrays.js
@@ -17,3 +17,20 @@ var random = [34, true, "sammy"]; //can mix data values
 //push adds a value to the end of array 
 //pop removes a value from the end of an array
 
+var dogColors = ["red", "orange", "yellow""];
+dogColors.push("indigo");
+dogColors.push("violet");
+
+dogColors.pop(); //will return the last item in an array
+
+//shift and unshift 
+//shift adds items to the front of an array
+//unshift adds items to the begining of an array
+
+dogColors.unshift("infrared");
+
+var col = dogColors.shift(); //remturns a value that can be stored in a variable
+
+
+
+
