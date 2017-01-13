91241345bef9d2fc1b02d11bcb1cdba16321eb00
George Higbie <gehigbie@gmail.com>
2017-01-13 16:41:34 -0700

    added comments

--- arrays.js
+++ arrays.js
@@ -12,3 +12,8 @@ colors[10] = "violet"; //this leaves a lot of empty values "undefined"
 var friends = new Array(); //creates a new array
 
 var random = [34, true, "sammy"]; //can mix data values
+
+// array methods: push and pop
+//push adds a value to the end of array 
+//pop removes a value from the end of an array
+
